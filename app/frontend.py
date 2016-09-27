93f47eb1372a61bfd2484108aa15aa1e1aa4124b
hestati <hestati@kaist.ac.kr>
2016-09-28 04:32:59 +0900

    add myequation page

--- app/frontend.py
+++ app/frontend.py
@@ -1,12 +1,14 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, send_from_directory, redirect, url_for
 
 app = Flask(__name__)
 frontend = Blueprint('frontend', __name__)
-
-
 
 @frontend.route('/', methods=['GET'], defaults={'s':''})
 @frontend.route('/<string:s>', methods=['GET'])
 def main(s):
     return render_template("main.html")
+
+@frontend.route('/formula/<path:path>', methods=['GET'])
+def send_file(path):
+    return redirect(url_for('static', filename='formula/' + path))
 
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -5,20 +5,24 @@ var browserHistory = ReactRouter.browserHistory;
 var IndexRedirect = ReactRouter.IndexRedirect;
 var Link = ReactRouter.Link;
 var Redirect = ReactRouter.Redirect;
+
 var Nav = ReactBootstrap.Nav;
 var Navbar = ReactBootstrap.Navbar;
 var NavItem = ReactBootstrap.NavItem;
 var Col = ReactBootstrap.Col;
 var PageHeader = ReactBootstrap.PageHeader;
-var LinkContainer = ReactRouterBootstrap.LinkContainer;
 var Jumbotron = ReactBootstrap.Jumbotron;
 var Button = ReactBootstrap.Button;
 var Modal = ReactBootstrap.Modal;
+var Panel = ReactBootstrap.Panel;
+var Row = ReactBootstrap.Row;
+var Label = ReactBootstrap.Label;
+var Thumbnail = ReactBootstrap.Thumbnail;
+
+var LinkContainer = ReactRouterBootstrap.LinkContainer;
 
 // temp values.
-const ACTIVE = { color: 'red' }
 var is_login = 0;
-var show = 0;
 
 class App extends React.Component {
 
@@ -102,7 +106,71 @@ class NewEquation extends React.Component {
     return (<h1>Ang Gimotti!</h1>);
   }
 }
+
+// show
+class Show extends React.Component {
+  render() {
+    var idx = window.location.hash.split('#')[1];
+//    $.post("/api");
+    var get = '{"latex": "x^2+1", "ci": "/formula/1.gif", "name": "MyNewEquation", "wi": "/formula/1.gif", "hangul": "{x}^{2}+1"}';
+    var data = JSON.parse(get);
+    return (
+        <Col xs={10} xsOffset={1}>
+        <PageHeader id="name">{data['name']}</PageHeader>
+        <Label bsStyle="primary">Your input</Label><br />
+        <img src={data['wi']} /><br />
+        <Label>Converted Result</Label><br />
+        <img src={data['ci']} />
+        <hr />
+        <Panel header="LaTex" bsStyle="primary">{data['latex']}</Panel><br />
+        <Panel header="Word" bsStyle="success">{data['hangul']}</Panel><br />
+        <Panel header="Hangul" bsStyle="info">{data['hangul']}</Panel><br />
+        </Col>
+        );
+  }
+}
 // my equation
+
+var Equation_gal = React.createClass({
+  render: function()
+  {
+    return(
+      <Col xs={6} md={4}>
+        <div className="card">
+          <h3>{this.props.name}</h3>
+          <img src={this.props.url} />
+          <p>
+          <LinkContainer to={"show#" + this.props.idx}>
+            <Button bsStyle="primary">Show</Button>
+          </LinkContainer>
+            &nbsp;
+          <LinkContainer to={"share#" + this.props.idx}>
+            <Button bsStyle="info">Share</Button>
+          </LinkContainer>
+            &nbsp;
+          <LinkContainer to={"delete#" + this.props.idx}>
+            <Button bsStyle="danger">Delete</Button>
+          </LinkContainer>
+          </p>
+        </div>
+      </Col>
+      );
+  }
+});
+
+class MyEquation extends React.Component {
+  render() {
+    return (
+        <Row>
+          <Col xs={10} xsOffset={1}>
+            <Equation_gal name="MyEquation" url="/formula/1.gif" idx={1}></Equation_gal>
+            <Equation_gal name="Calculus1" url="/formula/2.gif" idx={2}></Equation_gal>
+            <Equation_gal name="Calculus2" url="/formula/3.gif" idx={3}></Equation_gal>
+          </Col>
+        </Row>
+        )
+  }
+}
 // signin
 // signup
 // mypage
@@ -115,12 +183,16 @@ function logout(nextState, replaceState) {
 }
 
 
+
+
 ReactDOM.render(<Router history = {browserHistory}>
       <Route path = "/" component = {App}>
          <IndexRedirect to = "home" />
          <Route path = "home" component = {Home} />
          <Route path = "about" component = {About} />
          <Route path = "NewEquation" component = {NewEquation} />
+         <Route path = "MyEquation" component = {MyEquation} />
+         <Route path = "show" component = {Show} />
          <Route path = "logout" component = {None} onEnter={logout} />
            
       </Route>
